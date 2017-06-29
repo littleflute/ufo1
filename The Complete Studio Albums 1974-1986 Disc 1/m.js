5e4d1ccbc0cb164e82d701631c702669c2f33eff
littleflute <13699175164@sina.cn>
2017-06-29 17:51:48 -0500

    Add files via upload

---
+++ The Complete Studio Albums 1974-1986 Disc 1/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Oh My.mp3";		
+f[2]	= "02 Crystal Light.mp3";		
+f[3]	= "03 Doctor Doctor.mp3";		
+f[4]	= "04 Space Child.mp3";		
+f[5]	= "05 Rock Bottom.mp3";		
+f[6]	= "06 Too Young to No.mp3";		
+f[7]	= "07 Time on My Hands.mp3";		
+f[8]	= "08 Built for Comfort.mp3";		
+f[9]	= "09 Lipstick Traces.mp3";		
+f[10]	= "10 Queen of the Deep.mp3";		
+f[11]	= "11 Sixteen [-][Demo Version].mp3";		
+f[12]	= "12 Oh My [-][Demo Version].mp3";		
+f[13]	= "13 Give Her the Gun [-][A-Side].mp3";		
+f[14]	= "14 Sweet Little Thing [-][B-Side].mp3";		
+f[15]	= "15 Sixteen [-][Outtake][Take].mp3";		
